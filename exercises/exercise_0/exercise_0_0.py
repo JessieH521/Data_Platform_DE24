ba513d3bc7f78ec98dd5a1555448bc34811de34d
Jie Hou <namehb@gmail.com>
2025-01-14 16:40:47 +0100

    plotting seaborn matplotlib

--- exercises/exercise_0/exercise_0_0.py
+++ exercises/exercise_0/exercise_0_0.py
@@ -1,7 +1,17 @@
+# Solving 
+
+
+# 0. User input for ETL Parameters - Ask the user for 2 inputs:
+# source file path
+# destination file path
+
 from pathlib import Path
 
 source_file_path = input("Enter source file absolute path:")
 destination_file_path = input("Enter destination file absolute path:")
+
+# /Users/apple/Data_Platform/github/Data_Platform_DE24/exercises/exercise_0/input_data.csv
+# /Users/apple/Data_Platform/github/Data_Platform_DE24/exercises/exercise_0/output_data.csv
 
 print(f"source: {source_file_path}")
 print(f"destination: {destination_file_path}")
